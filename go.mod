d1dcedb6b6579d345ef6596783d3492a559dfd8f
Serhii Melnik <melnikdev@gmail.com>
2025-02-10 01:54:24 +0200

    Added dependencies for sending e-mail

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/melnikdev/book-mail
 
 go 1.22.2
+
+require (
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	gomail "gopkg.in/mail.v2"
+)
 
 func main() {
-	fmt.Println("Start")
+	// Create a new message
+	message := gomail.NewMessage()
+
+	// Set email headers
+	message.SetHeader("From", "youremail@email.com")
+	message.SetHeader("To", "recipient1@email.com")
+	message.SetHeader("Subject", "Hello from the Mailtrap team")
+
+	// Set email body
+	message.SetBody("text/plain", "This is the Test Body")
+
+	// Set up the SMTP dialer
+	dialer := gomail.NewDialer("sandbox.smtp.mailtrap.io", 587, "***", "***")
+
+	// Send the email
+	if err := dialer.DialAndSend(message); err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	} else {
+		fmt.Println("Email sent successfully!")
+	}
 }
